492a21019c7416f63d46fa4fb619af5fe0027024
Derek Thompson <derekallenthompson@gmail.com>
2020-06-02 22:03:50 -0500

    Finally got properties implemented into the DiceRoller and got properties passing properly from the test rolling page

--- src/components/DiceRoller.vue
+++ src/components/DiceRoller.vue
@@ -1,51 +1,41 @@
 <template>
-    <div class="card">
-        <div class="form-group">
-            <label for="num_dice">Number of Dice</label>
-            <input class="form-input" type="number" id="num_dice" name="num_dice" min="1" value="1">
-        </div>
-        <div class="form-group">
-            <label for="die_select">Die</label>
-            <select class="form-input" name="die_select" id="die_select">
-                <option value="2">D2</option>
-                <option value="3">D3</option>
-                <option value="4">D4</option>
-                <option value="6">D6</option>
-                <option value="8">D8</option>
-                <option value="10">D10</option>
-                <option value="12">D12</option>
-                <option value="20" selected>D20</option>
-                <option value="100">D100</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="modifier">Modifier</label>
-            <input class="form-input" type="number" id="modifier" name="modifier" value="0">
-        </div>
-        <div class="form-group">
-            <button type="button" @click="RollDice">Roll Dice</button>
-        </div>
-        <div id="result-div">
-            <h3>Total: <span v-html="results"></span></h3>
-        </div>
+    <div>
+        <button @click="rollDice">Roll Dice</button>
+        <h3>{{numDice}}d{{dieType}}+{{modifier}} <span v-html="this.results"></span></h3>
     </div>
 </template>
 
 <script>
 export default {
     name: 'DiceRoller',
+    props: {
+        numDice: {
+            type: Number,
+            required: true,
+            default: 1
+        },
+        dieType: {
+            type: Number,
+            required: true,
+            default: 20
+        },
+        modifier: {
+            type: Number,
+            default: 0
+        }
+    },
     data () {
         return {
             results: 0
         }
     },
     methods: {
-        RollDice() {
+        rollDice() {
             let result = 0
-            for (let i=0; i < Number(num_dice.value); i++) {
-                result += Math.floor(Math.random()*Number(die_select.value)) + 1
+            for (let i=0; i < this.numDice; i++) {
+                result += Math.floor(Math.random()* this.dieType) + 1
             }
-            result += Number(modifier.value)
+            result += this.modifier
             this.results = result
         }
     }
@@ -55,6 +45,7 @@ export default {
 <style scoped>
 .card {
     display: inline-block;
+    width: auto;
     background: #FFF;
     border: 1px solid black;
     border-radius: .5em;
