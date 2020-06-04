4408bbb67fc9e9a96b4e11fd3e5d69aeba67944e
Michael Daffin <michael@daffin.io>
2020-06-04 01:34:17 +0100

    Adds testing framework jest and a simple test for Ability component

---
+++ src/components/Ability.test.js
@@ -0,0 +1,31 @@
+import { shallowMount } from "@vue/test-utils";
+import Ability from "@/components/Ability.vue";
+
+describe("Ability", () => {
+  it.each([
+    [0, "-5"],
+    [1, "-5"],
+    [2, "-4"],
+    [3, "-4"],
+    [4, "-3"],
+    [5, "-3"],
+    [6, "-2"],
+    [7, "-2"],
+    [8, "-1"],
+    [9, "-1"],
+    [10, "+0"],
+    [11, "+0"],
+    [12, "+1"],
+    [13, "+1"],
+    [14, "+2"],
+    [15, "+2"],
+    [16, "+3"],
+    [17, "+3"],
+    [18, "+4"],
+    [19, "+4"],
+    [20, "+5"],
+  ])("value %i should create mod %i ((value - 10)/2) rounded down", (value, mod) => {
+    const wrapper = shallowMount(Ability, { propsData: { value } });
+    expect(wrapper.get(".mod").text()).toBe(mod);
+  });
+});
