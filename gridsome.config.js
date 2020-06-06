08b3bfb942c11b34921e04a08e333b517f1a31cd
Michael Daffin <michael@daffin.io>
2020-06-06 14:37:24 +0100

    Adds lints and audit checks (#6)
    And fixes the linting issues for all files so they are now all processed
    by eslint.

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,32 +5,30 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'RPG Tools',
+  siteName: "RPG Tools",
   plugins: [
     {
-      use: '@gridsome/source-filesystem',
+      use: "@gridsome/source-filesystem",
       options: {
-        path: 'content/characters/**/*.md',
-        typeName: 'Character',
-        route: '/character/:name'
-      }
+        path: "content/characters/**/*.md",
+        typeName: "Character",
+        route: "/character/:name",
+      },
     },
     {
-      use: '@gridsome/source-filesystem',
+      use: "@gridsome/source-filesystem",
       options: {
-        path: 'content/races/**/*.md',
-        typeName: 'Race',
-        route: '/race/:name'
-      }
-    }
+        path: "content/races/**/*.md",
+        typeName: "Race",
+        route: "/race/:name",
+      },
+    },
   ],
   transformers: {
     remark: {
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      plugins: [
-        '@gridsome/remark-prismjs'
-      ]
-    }
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+      plugins: ["@gridsome/remark-prismjs"],
+    },
   },
-}
+};
