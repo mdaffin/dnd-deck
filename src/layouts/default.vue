4fd430159dbd5ab26906a7aee6ff5b8f00e4ad2c
Derek Thompson <derekallenthompson@gmail.com>
2020-06-21 06:22:36 -0500

    Css work (#31)
    * Implemented slot functionality for Dice to allow them to be used inline and added bounds tests
    
    * rebasing
    
    * is this going to finally let me work again
    
    * Might finally have got this working. Blew up and rebuilt the branch
    
    * Fixes merge conflict in dice
    
    * Reverts changing ~ to @
    
    Co-authored-by: Michael Daffin <michael@daffin.io>

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,16 +1,46 @@
 <template>
   <div class="layout">
     <header class="header">
-      <strong>
-        <n-link to="/">dnd-deck</n-link>
-      </strong>
       <nav class="nav">
+        <strong>
+          <n-link to="/">DND-DECK</n-link>
+        </strong>
         <n-link class="nav__link" to="/">Home</n-link>
-        <n-link class="nav__link" to="/dice-roll-test">Dice Roller</n-link>
+        <n-link class="nav__link" to="/dice">Dice Roller</n-link>
       </nav>
     </header>
     <nuxt />
   </div>
 </template>
 
-<style></style>
+<style scoped>
+div,
+article,
+header,
+nav,
+form,
+a,
+main {
+  font-family: sans-serif;
+  color: #030303;
+  margin: 0;
+  padding: 0;
+}
+
+header {
+  background-color: #f1f1dee0;
+  padding: 2em 0;
+  justify-content: space-evenly;
+  border-bottom: 5px solid #930c10;
+}
+
+nav {
+  display: flex;
+  justify-content: flex-start;
+}
+
+a {
+  text-decoration: none;
+  margin: 0 0.5em;
+}
+</style>
