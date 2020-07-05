310eacc6405bff9eaad5bbf80ad9918b11dbfdd2
Michael Daffin <michael@daffin.io>
2020-07-05 17:50:20 +0100

    Improves the checkbox and orginaises the files abit (#45)

---
+++ src/components/character/actions/ProficiencyMarker.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input
+      readonly
+      type="checkbox"
+      :checked="checked"
+      onclick="return false;"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    checked: { type: Boolean },
+  },
+};
+</script>
+
+<style scoped>
+input {
+  appearance: none;
+  border: 2px solid #222;
+  border-radius: 100%;
+  padding: 4px;
+  display: inline-block;
+  position: relative;
+}
+
+input:checked {
+  background-color: #222;
+}
+</style>
---
+++ src/components/character/index.js
@@ -0,0 +1,6 @@
+import Abilities from "./abilities/Abilities";
+import Details from "./details/Details";
+import ProficiencyBonus from "./proficiencybonus/ProficiencyBonus";
+import Actions from "./actions/Actions";
+
+export default { Abilities, Details, ProficiencyBonus, Actions };
