98c0dc7062480e75576bdb8152f69d6f00b7c058
Michael Daffin <michael@daffin.io>
2020-07-05 02:02:43 +0100

    Adds stub saving throws and skills (#42)

---
+++ src/components/character/ProficiencyBonus.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <input readonly :value="value" />
+    <label>Proficiency Bonus</label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  border: 2px solid #eee;
+  transition: 0.3s;
+  border-radius: 10px;
+  margin: 8px;
+}
+
+label {
+  margin-left: 8px;
+}
+
+input {
+  display: flex;
+  text-align: center;
+
+  background-color: #fff;
+  width: 32px;
+  height: 32px;
+
+  border: none;
+  border-right: 2px solid #eee;
+  transition: 0.3s;
+  border-radius: 10px;
+}
+</style>
---
+++ src/components/character/SavingThrow.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="row">
+    <input
+      readonly
+      type="checkbox"
+      :checked="value.proficiency != undefined"
+      onclick="return false;"
+    />
+    <input readonly :value="total" />
+    <label>{{ label }}</label>
+  </div>
+</template>
+
+<script>
+import Dice from "~/components/Dice.vue";
+export default {
+  props: {
+    value: { type: Object },
+    label: { type: String },
+  },
+  components: { Dice },
+  computed: {
+    total() {
+      return this.value.value + (this.value.proficiency || 0);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.row {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+label {
+  margin-left: 8px;
+}
+
+input {
+  text-align: center;
+
+  background-color: #fff;
+  width: 32px;
+  height: 32px;
+
+  border: none;
+  border-right: 2px solid #eee;
+  transition: 0.3s;
+  border-radius: 10px;
+}
+</style>
