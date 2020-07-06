5f18525d677f2ceb8194502fc3037b3baf97733a
Michael Daffin <michael@daffin.io>
2020-07-06 01:20:24 +0100

    Adds basic stubbed combat stats

---
+++ src/components/character/combat/Card.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <slot></slot>
+    <Label>{{ label }}</Label>
+  </div>
+</template>
+
+<script>
+import Label from "~/components/basic/Label.vue";
+export default {
+  props: {
+    label: { type: String },
+  },
+  components: { Label },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  border: 2px solid #eee;
+  transition: 0.3s;
+  border-radius: 10px;
+  margin: 8px;
+  padding: 8px;
+  text-align: center;
+}
+
+.ability:hover {
+  background-color: #eee;
+}
+
+.value {
+  font-size: 2.5rem;
+}
+
+label {
+  font-size: 0.6rem;
+  color: #666;
+}
+</style>
--- src/components/character/index.js
+++ src/components/character/index.js
@@ -2,5 +2,6 @@ import Abilities from "./abilities/Abilities";
 import Details from "./details/Details";
 import ProficiencyBonus from "./proficiencybonus/ProficiencyBonus";
 import Actions from "./actions/Actions";
+import Combat from "./combat/Combat";
 
-export default { Abilities, Details, ProficiencyBonus, Actions };
+export default { Abilities, Details, ProficiencyBonus, Actions, Combat };
